66e42109a7727aa55ff6572e523d57b8f759c418
andmarek <andmarek@outlook.com>
2020-01-06 14:41:18 -0600

    changed pikachu's elemental type to electric

--- pokemon.py
+++ pokemon.py
@@ -7,17 +7,17 @@ class Pokemon:
         self.personal_name = personal_name
         self.elemental_type = elemental_type
 
-    # equivalent to javas toString kind of 
+    # equivalent to javas toString kind of
     def __str__(self):
         return " POKEMON NAME: %s\n PERSONAL NAME: %s\n ELEMENTAL TYPE: %s\n" % (self.name, self.personal_name, self.elemental_type.name)
-    
+
 
 
 def main():
-    electric = ElementalType(name="electric", attack_effective=["flying", "water"], attack_weakness=["dragon", "electric", "grass", "ground"], 
+    electric = ElementalType(name="electric", attack_effective=["flying", "water"], attack_weakness=["dragon", "electric", "grass", "ground"],
     defense_ineffective=["electric", "flying"], defense_weakness=["ground"])
 
-    pika = Pokemon(name="pikachu", personal_name="pwn man", elemental_type=ElementalType())
+    pika = Pokemon(name="pikachu", personal_name="pwn man", elemental_type=electric)
     print(pika)
 
 if __name__== "__main__":
